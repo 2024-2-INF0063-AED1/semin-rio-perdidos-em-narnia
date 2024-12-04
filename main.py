28aca440ce4db6d7b5e55fae1678432258d4aec8
vhaust <50644829+vhaust@users.noreply.github.com>
2024-12-03 22:38:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from geopy.geocoders import Nominatim
+from geopy.distance import geodesic
 
+def fetchCoordinates(postcode):
+    geolocator = Nominatim(user_agent="Djikstra")
+    location = geolocator.geocode(postcode)
+    coordinates = (location.latitude, location.longitude)
+    return coordinates
+
+def calcDistance(a, b):
+    distance = geodesic((a[0], a[1]), (b[0], b[1]))
+    return distance
+
+# api fetch
+# print(calcDistance(fetchCoordinates("00000000"), fetchCoordinates("00000000")))

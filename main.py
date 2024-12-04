65c09729975f53000c10095cf0e01cb25db1b137
vhaust <50644829+vhaust@users.noreply.github.com>
2024-12-04 19:45:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,85 @@
-from geopy.geocoders import Nominatim
+import heapq
+import networkx as nx
+import matplotlib.pyplot as plt
 from geopy.distance import geodesic
-from queue import PriorityQueue
 import itertools
 
-def fetchCoordinates(postcode):
+def addEdge(graph, u, v, weight):
+    if u not in graph:
+        graph[u] = {}
+    if v not in graph:
+        graph[v] = {}
+    graph[u][v] = weight
+    graph[v][u] = weight
+
+def calcDistance(coord_a, coord_b):
+    distance = geodesic(coord_a, coord_b).km
+    return distance
+
+def dijkstra(graph, start, end):
+    priority_queue = []
+    distances = {vertex: float('infinity') for vertex in graph}
+    previous_vertices = {vertex: None for vertex in graph}
+    distances[start] = 0
+    heapq.heappush(priority_queue, (0, start))
+
+    while priority_queue:
+        current_distance, current_vertex = heapq.heappop(priority_queue)
+
+        if current_vertex == end:
+            break
+
+        if current_distance > distances[current_vertex]:
+            continue
+
+        for neighbor, weight in graph[current_vertex].items():
+            distance = current_distance + weight
+
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                previous_vertices[neighbor] = current_vertex
+                heapq.heappush(priority_queue, (distance, neighbor))
+
+    path = []
+    current_vertex = end
+    while current_vertex is not None:
+        path.append(current_vertex)
+        current_vertex = previous_vertices[current_vertex]
+    path.reverse()
+
+    return distances[end], path
+
+def fetchLocationWip(postcode):
     geolocator = Nominatim(user_agent="Djikstra")
     location = geolocator.geocode(postcode)
+    return location
+
+def parseCoordinatesWip(location):
     coordinates = (location.latitude, location.longitude)
     return coordinates
 
-def calcDistance(a, b):
-    distance = geodesic((a[0], a[1]), (b[0], b[1]))
-    return distance
+def plotGraph(graph, path):
 
-def calcDistanceAlt(a, b):
-    distance = geodesic((a), (b))
-    return distance
+    G = nx.Graph()
 
-def readFile(filename):
-    with open(filename, 'r') as file:
-        for line in file:
-            print((line.rstrip('\n')))
+    for u in graph:
+        for v, weight in graph[u].items():
+            G.add_edge(u, v, weight=weight)
 
-# api fetch
-# print(calcDistance(fetchCoordinates("74605010"), fetchCoordinates("74690900")))
+    pos = nx.spring_layout(G)
 
-# hardcoded fetch
-# print(calcDistance((-16.6747174, -49.2354413), (-16.60252115, -49.2690862)))
+    nx.draw(G, pos, with_labels=True, node_size=2000, node_color='lightblue', font_size=10, font_weight='bold')
 
-#readFile('file.txt')
+    edges = G.edges()
+    weights = nx.get_edge_attributes(G, 'weight')
+    edge_labels = {edge: f"{weights[edge]:.2f}" for edge in edges}
+
+    path_edges = list(zip(path, path[1:]))
+    nx.draw_networkx_edges(G, pos, edgelist=path_edges, edge_color='red', width=2)
+
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+
+    plt.show()
 
 places = [('A', '-80.713937, 57.406936'),
 ('B', '38.67153, 87.039646'),
@@ -36,8 +87,26 @@ places = [('A', '-80.713937, 57.406936'),
 ('D', '-67.529481, 39.976455'),
 ('E', '22.698894, 36.315485'),
 ]
+combinations = itertools.combinations(places, 2)
 
-permutations = itertools.permutations(places, 2)
-for places in permutations:
-    print("\ncaminho: ", places[0][0], places[1][0])
-    print(calcDistanceAlt (places[0][1], places[1][1]) )
+data = []
+for places in combinations:
+    data.append( (places[0][0], places[1][0],
+    calcDistance (places[0][1], places[1][1]) ) )
+print(data)
+
+graph = {}
+for u, v, weight in data:
+    addEdge(graph, u, v, weight)
+
+start_vertex = 'A'
+end_vertex = 'E'
+distance, path = dijkstra(graph, start_vertex, end_vertex)
+for u, v, weight in data:
+    addEdge(graph, u, v, weight)
+
+# Exibindo o resultado
+print(f"A menor distância de {start_vertex} a {end_vertex} é {distance}")
+print(f"O caminho é: {' -> '.join(path)}")
+
+plotGraph(graph, path)
